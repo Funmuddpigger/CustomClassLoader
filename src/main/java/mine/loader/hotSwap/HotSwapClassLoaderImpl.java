faa8fece26b5237ef7a4c15fa4012343f6bea4b7
Li.junjie <v-lijj00.gd@chinatelecom.cn>
2023-12-21 17:46:16 +0800

    2023-12-21 1. commit HotSwap ClassLoader impl

---
+++ src/main/java/mine/loader/hotSwap/HotSwapClassLoaderImpl.java
@@ -0,0 +1,69 @@
+package mine.loader.hotSwap;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * @author ：Administrator
+ * @description：TODO
+ * @date ：2023/12/21 17:17
+ */
+public class HotSwapClassLoaderImpl extends ClassLoader {
+    //系统路径
+    private String rootPath;
+    //加载的类
+    private List<String> clazzs;
+
+    public HotSwapClassLoaderImpl(String classPath,String... clazzs){
+        this.rootPath = classPath;
+    }
+
+    /**
+     * 加载class路径文件
+     * @param file
+     */
+    public void loadClassPath(File file) throws IOException {
+        //如果是目录
+        if(file.isDirectory()&&file.listFiles()!=null){
+            //遍历
+            for(File file0 : file.listFiles()){
+                loadClassPath(file0);
+            }
+        }
+        //如果是文件，且是class文件则加载
+        else{
+            String fileName = file.getName();
+            String filePath = file.getPath();
+            //截取文件后缀名
+            String endName = fileName.substring(fileName.lastIndexOf(",") + 1);
+            //判断class类型
+            if("class".equals(endName)){
+                //加载class文件
+                FileInputStream fileInputStream = new FileInputStream(file);
+                //读取文件
+                byte[] bytes = new byte[(int) file.length()];
+                fileInputStream.read(bytes);
+                //转换路径
+                String className = filePathToClassName(filePath);
+                clazzs.add(className);
+                defineClass(className,bytes,0,bytes.length);
+            }
+        }
+
+    }
+
+    private String filePathToClassName(String filePath) {
+        //第一个replace先把文件根路径替换掉了，第二个replace把\\路径替换为.
+        String className = filePath.replace(rootPath, "").replaceAll("\\\\", ".");
+        className = className.substring(0, className.lastIndexOf("."));
+        className = className.substring(1);
+        return className;
+    }
+
+    @Override
+    public Class<?> loadClass(String name) throws ClassNotFoundException {
+        return super.loadClass(name);
+    }
+}
